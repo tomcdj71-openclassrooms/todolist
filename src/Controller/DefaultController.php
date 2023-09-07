d8f5d19ec767651f4465a17f366b26217582ae95
tomcdj71 <contact.tomc@yahoo.fr>
2023-09-07 15:21:54 +0200

    refactor(app): 33 use final classes
    Changes made:
    - use final classes for the controller
    - improve code quality (phpinsights)
    
    Closes: 33

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,7 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController extends AbstractController
+final class DefaultController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
     public function indexAction(): Response
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,17 +9,21 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class SecurityController extends AbstractController
+final class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'login')]
-    public function loginAction(AuthenticationUtils $authenticationUtils): Response
-    {
+    public function loginAction(
+        AuthenticationUtils $authenticationUtils
+    ): Response {
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render(
             'security/login.html.twig',
-            ['last_username' => $lastUsername, 'error' => $error]
+            [
+                'last_username' => $lastUsername,
+                'error' => $error,
+            ]
         );
     }
 
