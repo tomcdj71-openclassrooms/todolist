760fccf414e9edf4d3f5c949b497b0759711c177
tomcdj71 <contact.tomc@yahoo.fr>
2023-09-07 15:08:35 +0200

    refactor(app): #31 refactor
    heavy use of phpinsights to improve code quality

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -5,18 +5,36 @@ declare(strict_types=1);
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
-class UserType extends AbstractType
+final class UserType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
+    /**
+     * Builds the form.
+     *
+     * @param FormBuilderInterface $builder The form builder
+     * @param array<string, mixed> $options an array of options
+     *
+     * @phpcsSuppress SlevomatCodingStandard.Functions.UnusedParameter
+     */
+    public function buildForm(
+        FormBuilderInterface $builder,
+        array $options
+    ): void {
         $builder
-            ->add('username', TextType::class, ['label' => "Nom d'utilisateur"])
+            ->add(
+                'username',
+                TextType::class,
+                [
+                    'label' => "Nom d'utilisateur",
+                ]
+            )
             ->add(
                 'password',
                 RepeatedType::class,
@@ -24,10 +42,47 @@ class UserType extends AbstractType
                     'type' => PasswordType::class,
                     'invalid_message' => 'Les deux mots de passe doivent correspondre.',
                     'required' => true,
-                    'first_options' => ['label' => 'Mot de passe'],
-                    'second_options' => ['label' => 'Tapez le mot de passe à nouveau'],
+                    'first_options' => [
+                        'label' => 'Mot de passe',
+                    ],
+                    'second_options' => [
+                        'label' => 'Tapez le mot de passe à nouveau',
+                    ],
                 ]
             )
-            ->add('email', EmailType::class, ['label' => 'Adresse email']);
+            ->add(
+                'roles',
+                ChoiceType::class,
+                [
+                    'choices' => [
+                        'Utilisateur' => 'ROLE_USER',
+                        'Administrateur' => 'ROLE_ADMIN',
+                    ],
+                    'label' => 'Choisissez un rôle',
+                    'required' => true,
+                    'multiple' => false,
+                ]
+            )
+            ->add(
+                'email',
+                EmailType::class,
+                [
+                    'label' => 'Adresse email',
+                ]
+            );
+
+        $builder->get('roles')
+            ->addModelTransformer(
+                new CallbackTransformer(
+                    static function ($rolesArray) {
+                        // transform the array to a string
+                        return count($rolesArray) > 0 ? $rolesArray[0] : null;
+                    },
+                    static function ($rolesString) {
+                        // transform the string back to an array
+                        return [$rolesString];
+                    }
+                )
+            );
     }
 }
