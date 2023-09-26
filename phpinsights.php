7f6aa9ffa9189e97f0c8c1f4a4d30924a5ae83e2
tomcdj71 <contact.tomc@yahoo.fr>
2023-09-26 08:58:59 +0200

    chore(app): #38 update composer dependencies
    - update packages to latest versions
    - update tools configurations
    
    Closes: #38

---
+++ phpinsights.php
@@ -0,0 +1,131 @@
+<?php
+
+declare(strict_types=1);
+
+use NunoMaduro\PhpInsights\Domain\Sniffs\ForbiddenSetterSniff;
+use SlevomatCodingStandard\Sniffs\Functions\UnusedParameterSniff;
+use SlevomatCodingStandard\Sniffs\TypeHints\DisallowMixedTypeHintSniff;
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Default Preset
+    |--------------------------------------------------------------------------
+    |
+    | This option controls the default preset that will be used by PHP Insights
+    | to make your code reliable, simple, and clean. However, you can always
+    | adjust the `Metrics` and `Insights` below in this configuration file.
+    |
+    | Supported: "default", "laravel", "symfony", "magento2", "drupal"
+    |
+    */
+
+    'preset' => 'symfony',
+
+    /*
+    |--------------------------------------------------------------------------
+    | IDE
+    |--------------------------------------------------------------------------
+    |
+    | This options allow to add hyperlinks in your terminal to quickly open
+    | files in your favorite IDE while browsing your PhpInsights report.
+    |
+    | Supported: "textmate", "macvim", "emacs", "sublime", "phpstorm",
+    | "atom", "vscode".
+    |
+    | If you have another IDE that is not in this list but which provide an
+    | url-handler, you could fill this config with a pattern like this:
+    |
+    | myide://open?url=file://%f&line=%l
+    |
+    */
+
+    'ide' => 'vscode',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may adjust all the various `Insights` that will be used by PHP
+    | Insights. You can either add, remove or configure `Insights`. Keep in
+    | mind, that all added `Insights` must belong to a specific `Metric`.
+    |
+    */
+
+    'exclude' => [
+        dirname(__DIR__, 2).'/vendor',
+        dirname(__DIR__, 2).'/tools',
+    ],
+
+    'add' => [
+        //  ExampleMetric::class => [
+        //      ExampleInsight::class,
+        //  ]
+    ],
+
+    'remove' => [
+        AlphabeticallySortedUsesSniff::class,
+        DeclareStrictTypesSniff::class,
+        DisallowMixedTypeHintSniff::class,
+        DisallowShortTernaryOperatorSniff::class,
+        ForbiddenDefineFunctions::class,
+        ForbiddenFinalClasses::class,
+        ForbiddenTraits::class,
+        ParameterTypeHintSniff::class,
+        PropertyTypeHintSniff::class,
+        ReturnTypeHintSniff::class,
+        UselessFunctionDocCommentSniff::class,
+        UnnecessaryFinalModifierSniff::class,
+    ],
+
+    'config' => [
+        UnusedParameterSniff::class => [
+            'exclude' => [
+                dirname(__DIR__, 3).'/src/Form',
+            ],
+        ],
+        DisallowMixedTypeHintSniff::class => [
+            'exclude' => [
+                dirname(__DIR__, 3).'/src/Form',
+            ],
+        ],
+        ForbiddenSetterSniff::class => [
+            'exclude' => [
+                dirname(__DIR__, 3).'/src/Entity',
+            ],
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Requirements
+    |--------------------------------------------------------------------------
+    |
+    | Here you may define a level you want to reach per `Insights` category.
+    | When a score is lower than the minimum level defined, then an error
+    | code will be returned. This is optional and individually defined.
+    |
+    */
+
+    'requirements' => [
+        'min-quality' => 80,
+        'min-complexity' => 80,
+        'min-architecture' => 80,
+        'min-style' => 80,
+//        'disable-security-check' => false,
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Threads
+    |--------------------------------------------------------------------------
+    |
+    | Here you may adjust how many threads (core) PHPInsights can use to perform
+    | the analyse. This is optional, don't provide it and the tool will guess
+    | the max core number available. It accepts null value or integer > 0.
+    |
+    */
+
+    'threads' => 4,
+];
